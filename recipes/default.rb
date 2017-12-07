c693bf1eb159d22aae009abc4c5c657c204dea5f
Mike Juarez <mike@imgix.com>
2017-12-07 10:24:51 -0800

    load gem for compile time

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,5 +6,8 @@
 # Copyright 2014, Paul Czarkowski, Rackspace
 #
 
-chef_gem 'pleaserun'
+chef_gem 'pleaserun' do
+  compile_time true if respond_to?(:compile_time)
+  clear_sources true
+end
 require  'pleaserun/namespace'
